45d59d19bf121220db452ed4942d2d90c40d9d3c
Samuel Cox <samc1253@gmail.com>
2023-01-12 15:27:05 -0700

    Remove github insights from views to speed up load times for tests and production

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -45,34 +45,4 @@ RSpec.describe 'The Merchant Dashboard' do
       expect(Item.find(3).name).to appear_before(Item.find(4).name)
     end
   end
-
-  it 'displays the name of the github repo' do
-    visit merchant_dashboard_path(1)
-
-    expect(page).to have_content("sambcox/little-esty-shop")
-  end
-
-  it 'displays all contibutors usernames' do
-    visit merchant_dashboard_path(1)
-
-    expect(page).to have_content('sambcox')
-    expect(page).to have_content('this-is-joeking')
-    expect(page).to have_content('ryancanton')
-    expect(page).to have_content('Mike-Cummins')
-  end
-
-  it 'displays the number of commits next to each github username' do
-    visit merchant_dashboard_path(1)
-
-    expect(page).to have_content('sambcox 66 commits')
-    expect(page).to have_content('this-is-joeking 33 commits')
-    expect(page).to have_content('ryancanton 36 commits')
-    expect(page).to have_content('Mike-Cummins 37 commits')
-  end
-
-  it 'displays the number of merged PRs for all team members' do
-    visit merchant_dashboard_path(1)
-
-    expect(page).to have_content('54 Merged PRs')
-  end
 end
