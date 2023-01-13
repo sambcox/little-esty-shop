f9290c58a3847a79ca2c86b45afd51412cf06988
Samuel Cox <samc1253@gmail.com>
2023-01-13 14:20:34 -0700

    Create test, view, and routes for merchant bulk discount delete

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -21,4 +21,20 @@ RSpec.describe 'merchants bulk discounts index' do
     click_button 'Create a new bulk discount'
     expect(current_path).to eq(new_merchant_bulk_discount_path(@merchant_1))
   end
+
+  it 'has a button to delete each bulk discount' do
+    bulk_discount_test_seed_scenario_5
+    visit merchant_bulk_discounts_path(@merchant_1)
+    bulk_discount = @merchant_1.bulk_discounts.first
+
+    within("#bulk_discount_#{bulk_discount.id}") do
+      click_button 'Delete Bulk Discount'
+    end
+
+    expect(current_path).to eq(merchant_bulk_discounts_path(@merchant_1))
+    expect(page).to have_content(@merchant_1.bulk_discounts.last.quantity_threshold)
+    expect(page).to_not have_content(bulk_discount.quantity_threshold)
+    reload bulk_discount
+    expect(bulk_discount.exists?).to eq false
+  end
 end
--- spec/features/merchants/bulk_discounts/new_spec.rb
+++ spec/features/merchants/bulk_discounts/new_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'merchants bulk discount new' do
     expect(page).to have_link('10% discount when buying 5 or more of a specific item')
   end
 
-  it 'has a form to create a new bulk discount' do
+  it 'will not create bulk discount if a number is not correct' do
     bulk_discount_test_seed_scenario_5
     visit new_merchant_bulk_discount_path(@merchant_1)
 
