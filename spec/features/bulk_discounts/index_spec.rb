1f15362f95e7668cdc987559029b9e6b61a892fa
Mike Cummins <Cummins.Michael.R@gmail.com>
2023-01-15 16:02:19 -0800

    Complete user story 1

---
+++ spec/features/bulk_discounts/index_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Bulk Discounts Index' do
+  describe 'As a merchant when I visit my dashboard' do
+    before :each do
+      @merchant_1 = Merchant.first
+      @merchant_2 = Merchant.last
+      @discount_1 = @merchant_1.bulk_discounts.create!(percentage_discount: 0.30, quantity_threshold: 15)
+      @discount_2 = @merchant_1.bulk_discounts.create!(percentage_discount: 0.20, quantity_threshold: 10)
+      @discount_3 = @merchant_2.bulk_discounts.create!(percentage_discount: 0.10, quantity_threshold: 5)
+    end
+    it 'Has a link to view all bulk discounts' do
+      visit merchant_dashboard_path(@merchant_1.id)
+      click_link("View all Discounts")
+
+      expect(current_path).to eq(merchant_bulk_discounts_path(@merchant_1.id))
+      expect(page).to have_content("0.2 off when you buy 10")
+      expect(page).to have_content("0.3 off when you buy 15")
+      expect(page).to_not have_content("0.1 off when you buy 5")
+    end
+
+    it 'Has a link to view each discounts show page' do
+      visit merchant_bulk_discounts_path(@merchant_1.id)
+
+      click_on("0.3 off when you buy 15")
+
+      expect(current_path).to eq(merchant_bulk_discount_path(@merchant_1, @discount_1))
+    end
+  end
+end
